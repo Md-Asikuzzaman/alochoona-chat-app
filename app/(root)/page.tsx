12940a9c3d0f4a795dfb138604d3b4262ad1bf16
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-05-12 18:12:58 +0600

    fix online status bugs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -30,8 +30,7 @@ export default function Home() {
             status: "offline",
             id: user.id,
           });
-
-          // signOut();
+          signOut();
         }
       }
     };
