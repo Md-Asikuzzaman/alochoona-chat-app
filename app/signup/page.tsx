0697a9923db6ec263ba574f3c9f368c2e6ba7757
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-05-26 19:15:36 +0600

    fix: min-height for auth page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -65,7 +65,7 @@ const SignIn = () => {
 
   return (
     <section className="bg-zinc-200 dark:bg-gray-900">
-      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 h-screen lg:py-0">
         <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
