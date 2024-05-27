44a5c4325b2037604db1aaff477c65b8ad06e1a5
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-05-27 14:50:28 +0600

    style: fix color combination

--- app/components/ui/LogOutButton.tsx
+++ app/components/ui/LogOutButton.tsx
@@ -84,8 +84,8 @@ const LogOutButton = () => {
               onClick={handleClick}
               className="flex cursor-pointer items-center gap-3 rounded-md px-4 py-2 text-sm transition-colors hover:bg-rose-400"
             >
-              <AiOutlineLogout size={20} />
-              <p className="text-[#8318b4]">Log out</p>
+              <AiOutlineLogout size={20} className="text-slate-900" />
+              <p className="text-slate-900">Log out</p>
             </div>
           </motion.div>
         )}
