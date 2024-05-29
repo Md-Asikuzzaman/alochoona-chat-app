4abebc960bf30304796bfafa7e8163baece799d1
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-05-29 16:13:37 +0600

    fix: overflow issues

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,13 +7,11 @@ interface Props {
 
 const Layout: NextPage<Props> = ({ children }) => {
   return (
-    <section className="min-h-screen w-full bg-slate-900">
+    <section className="min-h-screen w-full overflow-hidden bg-slate-900">
       <div className="mx-auto lg:container">
         <div className="flex min-h-screen flex-1 bg-gray-100 lg:overflow-hidden lg:rounded-xl">
           <FriendList />
-          <div className="flex-1">
-            {children}
-          </div>
+          <div className="flex-1">{children}</div>
         </div>
       </div>
     </section>
